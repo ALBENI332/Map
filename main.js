6bb73a8a746d898d3b5acecabe8a094308d10c4a
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-10-30 19:32:29 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// const numbers = [1, 2, 3]; 
+ 
+// const doubledNumbers = numbers.map(number => { 
+//   return number * 2; 
+// }); 
+ 
+// console.log(doubledNumbers); 
+ 
+ 
+ 
+ 
+ 
+ 
+//________________________ 
+ 
+ 
+ 
+ 
+ 
+// const strings = ['привет', 'мир']; 
+ 
+// const upperCaseStrings = strings.map(str => { 
+//   return str.toUpperCase(); 
+// }); 
+ 
+// console.log(upperCaseStrings); 
+ 
+ 
+ 
+ 
+//______________________________ 
+ 
+ 
+ 
+ 
+ 
+// const numbers = [1, 2, 3, 4]; 
+ 
+// const stringNumbers = numbers.map(number => { 
+//   return number.toString(); 
+// }); 
+ 
+// console.log(stringNumbers); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//____________________________________ 
+ 
+ 
+ 
+ 
+ 
+// const animals = ['кот', 'собака', 'тигр']; 
+ 
+// const exclamatedAnimals = animals.map(animal => { 
+//   return animal + '!'; 
+// }); 
+ 
+// console.log(exclamatedAnimals); 
+ 
+ 
+ 
+ 
+ 
+//________________________________ 
+ 
+ 
+ 
+// const users = [ 
+//     { name: 'Алекс', age: 25 }, 
+//     { name: 'Мария', age: 17 }, 
+//     { name: 'Иван', age: 19 } 
+// ]; 
+ 
+// const names = users.map(user => { 
+//     return user.name; 
+// }); 
+ 
+// console.log(names);
